6e670fa7fbba16fa17910c116dbed51fd343413f
Cristian Guaman <cristian.guaman@icloud.com>
2021-03-29 22:37:00 -0500

    useEffect - optimizado

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -4,11 +4,9 @@ import { useState } from 'react';
 
 const GifGrid = ({ category }) => {
 
-  const [count, setCount] = useState(0);
-
-  /* useEffect(() => { //Se ejecuta esto cuando el componente se renderiza por 1ra vez
+  useEffect(() => { //Se ejecuta esto cuando el componente se renderiza por 1ra vez
     getGifs();
-  }, []); */
+  }, []);
 
   const getGifs = async () => {
     const url = "https://api.giphy.com/v1/gifs/search?limit=10&q=Rick ann morty&api_key=CTultUNDaOlOTkVSr2pou3ItXZVBWFxZ";
@@ -24,13 +22,9 @@ const GifGrid = ({ category }) => {
     })
     console.log(gifs);
   }
-
-  getGifs();
   return (
     <div>
       <h3>{category}</h3>
-      <h3>{count}</h3>
-      <button onClick={() => setCount(count + 1)} >click</button>
     </div>
   )
 }
