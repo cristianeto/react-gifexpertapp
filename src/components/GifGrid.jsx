35960b79a8658de815b05442e034fdb8cdc59e8a
Cristian Guaman <cristian.guaman@icloud.com>
2021-03-29 22:36:08 -0500

    useEffect - sin optimizarlo

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useEffect } from 'react';
+import { useState } from 'react';
 
 const GifGrid = ({ category }) => {
 
-  useEffect(() => { //Se ejecuta esto cuando el componente se renderiza por 1ra vez
+  const [count, setCount] = useState(0);
+
+  /* useEffect(() => { //Se ejecuta esto cuando el componente se renderiza por 1ra vez
     getGifs();
-  }, []);
+  }, []); */
 
   const getGifs = async () => {
     const url = "https://api.giphy.com/v1/gifs/search?limit=10&q=Rick ann morty&api_key=CTultUNDaOlOTkVSr2pou3ItXZVBWFxZ";
@@ -22,10 +25,12 @@ const GifGrid = ({ category }) => {
     console.log(gifs);
   }
 
-
+  getGifs();
   return (
     <div>
       <h3>{category}</h3>
+      <h3>{count}</h3>
+      <button onClick={() => setCount(count + 1)} >click</button>
     </div>
   )
 }
