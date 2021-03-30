92d6674062f9ed2b99c5cb1049b1ebead3d7a5f9
Cristian Guaman <cristian.guaman@icloud.com>
2021-03-29 21:53:36 -0500

    72. Comunicación entre componentes

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AddCategory from './components/AddCategory';
 
 const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One punch', 'Samurai', 'Dragon Ball']);
+
+  /*   const handleAdd = () => {
+      //setCategories(['HunterXHunter', ...categories]);
+      setCategories(cats => [...categories, 'HunterXHunter']);
+    }; */
   return (
     <>
       <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
       <hr />
+
+      <ol>
+        {
+          categories.map((category, index) => (
+            <li key={index}>{category}</li>
+          ))
+        }
+      </ol>
     </>
   )
 }
---
+++ src/components/AddCategory.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    console.log(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('submiting');
+    if (inputValue.trim().length > 2) {
+      setCategories(cats => [...cats, inputValue]);
+      setInputValue('');
+    }
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Write a category"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+    </form>
+  )
+}
+
+export default AddCategory
+
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired,
+}
